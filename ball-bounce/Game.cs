db8057dee405a69cd71528cd8335603515885415
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-03-03 16:28:26 -0500

    Move ball to own class

--- ball-bounce/Game.cs
+++ ball-bounce/Game.cs
@@ -3,57 +3,26 @@ using System;
 using System.Numerics;
 
 // The namespace your code is in.
-namespace MohawkGame2D
+namespace MohawkGame2D;
+
+/// <summary>
+///     Your game code goes inside this class!
+/// </summary>
+public class Game
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
-    public class Game
+    Ball ball = new Ball();
+
+    public void Setup()
     {
-        // Ball
-        Vector2 position = new Vector2(200, 50);
-        Vector2 velocity;
-        float radius = 25;
-        // "Physics"
-        Vector2 gravity = new Vector2(0, 8);
-        float forceKept = 0.50f; // 50%
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
-        public void Setup()
-        {
-            Window.SetTitle("Ball Bounce");
-            Window.SetSize(400, 800);
-            Window.TargetFPS = 60;
-        }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
-        public void Update()
-        {
-            Window.ClearBackground(Color.OffWhite);
-
-            // Move ball
-            velocity += gravity;
-            position += velocity * Time.DeltaTime;
-
-            // Check if we are below the screen height / bottom edge
-            if (position.Y + radius > Window.Height)
-            {
-                // Move ball up to touching bottom edge
-                position.Y = Window.Height - radius;
-                // Invert velocity to bounce up, scale velocity down a bit
-                velocity.Y = -velocity.Y * forceKept;
-            }
-
-            // Draw ball
-            Draw.LineSize = 1;
-            Draw.LineColor = Color.Black;
-            Draw.FillColor = Color.Red;
-            Draw.Circle(position, radius);
-        }
+        Window.SetTitle("Ball Bounce");
+        Window.SetSize(400, 800);
+        Window.TargetFPS = 60;
     }
 
+    public void Update()
+    {
+        Window.ClearBackground(Color.OffWhite);
+        ball.MoveBall();
+        ball.DrawBall();
+    }
 }
--- sine-cosine/Game.cs
+++ sine-cosine/Game.cs
@@ -11,14 +11,14 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        float angle = 0;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(400, 400);
         }
 
         /// <summary>
@@ -26,7 +26,30 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
 
+            //if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            //{
+            //    angle += 1;
+            //}
+            angle = Time.SecondsElapsed * MathF.Tau / 5;
+
+            // Screen centre
+            Vector2 centre = Window.Size / 2;
+            // Calculate point on circle, then draw something there
+            Vector2 offset = new Vector2(
+                MathF.Cos(angle) * 150,  // X
+                MathF.Sin(angle) *  50); // Y
+            Vector2 position = centre + offset;
+
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.Black;
+            Draw.Circle(centre, 3);
+            Draw.FillColor = Color.Green;
+            Draw.Circle(position, 25);
+
+            Text.Draw($"Angle: {angle}", 10, 10);
         }
     }
 
